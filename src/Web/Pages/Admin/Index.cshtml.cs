cc2e038f5a079a9be861895d6edbfc3eaaef2f29
ShadyNagy <info@shadynagy.com>
2024-06-13 11:18:17 +0300

    - Use Administrators string not the constants from the SharedBlazor.

--- src/Web/Pages/Admin/Index.cshtml.cs
+++ src/Web/Pages/Admin/Index.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Microsoft.eShopWeb.Web.Pages.Admin;
 
-[Authorize(Roles = BlazorShared.Authorization.Constants.Roles.ADMINISTRATORS)]
+[Authorize(Roles = "Administrators")]
 public class IndexModel : PageModel
 {
     public IndexModel()
