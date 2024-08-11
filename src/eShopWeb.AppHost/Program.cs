4331e951217c48e87db051124926412580c19535
ShadyNagy <info@shadynagy.com>
2024-08-11 22:02:33 +0300

    - Aspire projects are added.

---
+++ src/eShopWeb.AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var publicApi = builder.AddProject<Projects.PublicApi>(nameof(Projects.PublicApi).ToLower());
+
+builder
+    .AddProject<Projects.Web>(nameof(Projects.Web).ToLower())
+    .WithExternalHttpEndpoints()
+    .WithReference(publicApi);
+
+builder.Build().Run();
