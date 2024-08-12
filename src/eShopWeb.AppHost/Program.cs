300b003e4f05ed806c317fa4cf3f35b8463485df
ShadyNagy <info@shadynagy.com>
2024-08-12 05:52:58 +0300

    - refactored the AppHost

--- src/eShopWeb.AppHost/Program.cs
+++ src/eShopWeb.AppHost/Program.cs
@@ -1,15 +1,9 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var publicApi = builder.AddProject<Projects.PublicApi>(nameof(Projects.PublicApi).ToLower());
-
-var blazorAdmin = builder
-    .AddProject<Projects.BlazorAdmin>(nameof(Projects.BlazorAdmin).ToLower())
-    .WithExternalHttpEndpoints()
-    .WithReference(publicApi);
+builder
+    .AddProject<Projects.PublicApi>(nameof(Projects.PublicApi).ToLower());
 
 builder
-    .AddProject<Projects.Web>(nameof(Projects.Web).ToLower())
-    .WithExternalHttpEndpoints()
-    .WithReference(blazorAdmin);
+    .AddProject<Projects.Web>(nameof(Projects.Web).ToLower());
 
 builder.Build().Run();
