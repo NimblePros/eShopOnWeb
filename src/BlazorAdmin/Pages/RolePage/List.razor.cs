8a044257d10e3a1b3f947133abe12a6a89fbbf64
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-03-09 16:02:23 -0400

    Razor page placeholder in place for Roles

---
+++ src/BlazorAdmin/Pages/RolePage/List.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorAdmin.Helpers;
+using BlazorShared.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorAdmin.Pages.RolePage;
+
+public partial class List : BlazorComponent
+{
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+          
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+}
