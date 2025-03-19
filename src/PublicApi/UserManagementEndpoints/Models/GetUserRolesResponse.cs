49dbe9f155006c52d209d19817250a7f68652eb8
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-03-19 17:44:01 -0400

    Adding an endpoint to get the roles for a user

---
+++ src/PublicApi/UserManagementEndpoints/Models/GetUserRolesResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Microsoft.eShopWeb.PublicApi.UserManagementEndpoints.Models;
+
+public class GetUserRolesResponse : BaseResponse
+{
+
+    public GetUserRolesResponse(Guid correlationId) : base(correlationId)
+    {
+    }
+
+    public GetUserRolesResponse()
+    {
+    }
+
+    public List<string> Roles {  get; set; }
+}
