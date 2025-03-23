5d05a18f1dd295d61d5761f8b35aec477604017d
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-03-23 16:25:50 -0400

    Current user can't edit themselves; no one can edit default admin

--- src/BlazorAdmin/Pages/UserPage/List.razor.cs
+++ src/BlazorAdmin/Pages/UserPage/List.razor.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BlazorAdmin.Helpers;
 using BlazorAdmin.Interfaces;
 using BlazorAdmin.Models;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Logging;
 
 namespace BlazorAdmin.Pages.UserPage;
@@ -13,12 +15,23 @@ public partial class List : BlazorComponent
     public IUserManagementService UserManagementService{ get; set; }
     [Microsoft.AspNetCore.Components.Inject]
     ILogger<List> Logger { get; set; }
+    [Microsoft.AspNetCore.Components.Inject]
+    public AuthenticationStateProvider AuthProvider { get; set; }
 
     private List<User> _users = [];
+    private ClaimsPrincipal _currentUser = null;
     private Create CreateComponent { get; set; }
     private Delete DeleteComponent { get; set; }
     private Edit EditComponent { get; set; }
     
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        var getCurrentClaim = await AuthProvider.GetAuthenticationStateAsync();
+        _currentUser = getCurrentClaim.User;
+        Logger.LogInformation("Current User: {0}", _currentUser.Identity.Name);
+    }
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
@@ -30,6 +43,7 @@ public partial class List : BlazorComponent
         }
 
         await base.OnAfterRenderAsync(firstRender);
+
     }
 
     private async Task CreateClick()
