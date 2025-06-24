583985d5f4f4c8c7f6b0e8f25469e8ebc1b34a20
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-06-23 22:56:34 -0400

    Dealing with non-nullable warning

--- src/ApplicationCore/Entities/OrderAggregate/Events/OrderCreatedEvent.cs
+++ src/ApplicationCore/Entities/OrderAggregate/Events/OrderCreatedEvent.cs
@@ -8,13 +8,13 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate.Events;
 public class OrderCreatedEvent :IMessage
 {
     public int Id { get; init; }
-    public string BuyerId { get; init; }
+    public string? BuyerId { get; init; }
     public DateTimeOffset OrderDate { get; init; }
     public decimal Total { get; init; }
     public IReadOnlyCollection<OrderItem> Items { get; init; } = [];
     public OrderCreatedEvent() { }
 
-    public OrderCreatedEvent(int orderId, string buyerId, DateTimeOffset orderDate, decimal orderTotal, IReadOnlyCollection<OrderItem> orderItems) {
+    public OrderCreatedEvent(int orderId, string? buyerId, DateTimeOffset orderDate, decimal orderTotal, IReadOnlyCollection<OrderItem> orderItems) {
         Id = orderId;
         BuyerId = buyerId;
         OrderDate = orderDate;
